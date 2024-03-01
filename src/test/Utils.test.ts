81bbcf2e25d296e3e814f9c7561dfbb7582c40f4
Ryu <juker1141@gmail.com>
2024-03-01 16:19:25 +0800

    init: project setup

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,12 @@
+import { toUpperCase } from "../app/Utils";
+
+describe("Utils test suite", () => {
+  it("should return uppercase of valid string", () => {
+    const sut = toUpperCase;
+    const expected = "ABC";
+
+    const actual = sut("abc");
+
+    expect(actual).toBe(expected);
+  });
+});
