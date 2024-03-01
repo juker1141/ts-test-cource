4ab2f0275b967344e4a261462a8c3e903ca6175d
Ryu <juker1141@gmail.com>
2024-03-01 17:58:06 +0800

    update: add test

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   it("should return uppercase of valid string", () => {
@@ -8,5 +8,15 @@ describe("Utils test suite", () => {
     const actual = sut("abc");
 
     expect(actual).toBe(expected);
+  });
+
+  it("should return info for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
   });
 });
