e29e4317008771c1c294e2e7c9b28242fd855ba8
Ryu <juker1141@gmail.com>
2024-03-04 17:41:23 +0800

    update: jest hooks

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,6 +1,42 @@
-import { getStringInfo, toUpperCase } from "../app/Utils";
+import { StringUtils, getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
+  describe.only("StringUtils tests", () => {
+    let sut: StringUtils;
+
+    beforeEach(() => {
+      sut = new StringUtils();
+    });
+
+    it("Should return correct upperCase", () => {
+      const actual = sut.toUpperCase("abc");
+      expect(actual).toBe("ABC");
+    });
+
+    it("Should throw error on invalid argument - function", () => {
+      function expectError() {
+        const actual = sut.toUpperCase("");
+      }
+      expect(expectError).toThrow();
+      expect(expectError).toThrowError("Invalid argument!");
+    });
+
+    it("Should throw error on invalid argument - arrow function", () => {
+      expect(() => {
+        const actual = sut.toUpperCase("");
+      }).toThrowError("Invalid argument!");
+    });
+
+    it("Should throw error on invalid argument - try catch block", () => {
+      try {
+        sut.toUpperCase("");
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+        expect(error).toHaveProperty("message", "Invalid argument!");
+      }
+    });
+  });
+
   it("should return uppercase of valid string", () => {
     const sut = toUpperCase;
     const expected = "ABC";
@@ -10,13 +46,62 @@ describe("Utils test suite", () => {
     expect(actual).toBe(expected);
   });
 
-  it("should return info for valid string", () => {
-    const actual = getStringInfo("My-String");
+  describe.only("ToUpperCase example", () => {
+    it.each([
+      { input: "abc", expected: "ABC" },
+      { input: "My-String", expected: "MY-STRING" },
+      { input: "def", expected: "DEF" },
+    ])("$input toUpperCase should be $expected", ({ input, expected }) => {
+      const actual = toUpperCase(input);
+      expect(actual).toBe(expected);
+    });
+  });
 
-    expect(actual.lowerCase).toBe("my-string");
-    expect(actual.extraInfo).toEqual({});
+  describe("getStringInfo for arg My-String should", () => {
+    test("return right length", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.characters).toHaveLength(9);
+    });
 
-    expect(actual.characters.length).toBe(9);
-    expect(actual.characters).toHaveLength(9);
+    test("return right lower case", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.lowerCase).toBe("my-string");
+    });
+
+    test("return right lower case", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.upperCase).toBe("MY-STRING");
+    });
+
+    test("return right characters", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.characters.length).toBe(9);
+      expect(actual.characters).toHaveLength(9);
+      expect(actual.characters).toEqual([
+        "M",
+        "y",
+        "-",
+        "S",
+        "t",
+        "r",
+        "i",
+        "n",
+        "g",
+      ]);
+      expect(actual.characters).toContain("M");
+      expect(actual.characters).toEqual(
+        expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"])
+      );
+    });
+
+    test("return defind extra info", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.extraInfo).toBeDefined();
+    });
+
+    test("return right extra info", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.extraInfo).toEqual({});
+    });
   });
 });
