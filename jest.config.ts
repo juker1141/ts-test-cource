a3464a59e9da53e26128c68122c457e84f1761b0
Ryu <juker1141@gmail.com>
2024-03-26 14:05:19 +0800

    update: start write tdd with passwordChecker

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,15 @@
 import { Config } from "@jest/types";
 
+const baseDir = "<rootDir>/src/app/pass_checker";
+const baseTestDir = "<rootDir>/src/test/pass_checker";
+
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
-  watch: true,
+  collectCoverage: true,
+  collectCoverageFrom: [`${baseDir}/**/*.ts`],
+  testMatch: [`${baseTestDir}/**/*.ts`],
 };
 
 export default config;
